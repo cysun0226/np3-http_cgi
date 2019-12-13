3af5d1bbeb90b0928f8171c6cb0bab77a1aa82f6
cysun <chiayusun.cs08g@nctu.edu.tw>
2019-12-13 21:04:27 +0800

    finish np_client.cpp

--- cgi/np_client.cpp
+++ cgi/np_client.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <vector>
 #include <unistd.h>
+#include <boost/algorithm/string/replace.hpp>
 
 using namespace boost::asio;
 using namespace boost::asio::ip;
@@ -22,18 +23,104 @@ std::vector<std::string> cmds;
 int cmd_idx = 0;
 std::string session_id;
 
+const std::string webpage_template = R"(
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>NP Project 3 Console</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Code+Pro"
+      rel="stylesheet"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      href="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678068-terminal-512.png"
+    />
+    <style>
+      * {
+        font-family: 'Source Code Pro', monospace;
+        font-size: 1rem !important;
+      }
+      body {
+        background-color: #212529;
+      }
+      pre {
+        color: #cccccc;
+      }
+      b {
+        color: #ffffff;
+      }
+    </style>
+  </head>
+  <body>
+    <table class="table table-dark table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">nplinux7.cs.nctu.edu.tw:3333</th>
+          <th scope="col">nplinux8.cs.nctu.edu.tw:4444</th>
+          <th scope="col">nplinux9.cs.nctu.edu.tw:5555</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><pre id="s0" class="mb-0"></pre></td>
+          <td><pre id="s1" class="mb-0"></pre></td>
+          <td><pre id="s2" class="mb-0"></pre></td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
+)";
+
+void output_command(std::string sid, std::string cmd){
+    boost::replace_all(cmd, "\r", "");
+    // boost::replace_all(cmd, "\n", "&NewLine");
+    std::cout << "<script>document.getElementById('s" << 
+    sid << "').innerHTML += '<b>" << cmd << "&NewLine;</b>';</script>" << std::endl;
+    std::cout.flush();
+}
+
+void output_prompt(std::string sid){
+    std::cout << "<script>document.getElementById('s" << 
+    sid << "').innerHTML += '<b>" << "% " << "</b>';</script>" << std::endl;
+    std::cout.flush();
+}
+
+void output_shell(std::string sid, std::string str){
+    boost::replace_all(str, "\r", "");
+    boost::replace_all(str, "\n", "&NewLine;");
+    std::cout << "<script>document.getElementById('s" << sid << 
+                 "').innerHTML += '" << str <<"';</script>" << std::endl;
+    std::cout.flush();
+}
+
 void read_handler(const boost::system::error_code &ec,
   std::size_t bytes_transferred)
 {
   if (!ec)
   {
     std::string recv_str(bytes.data());
+    // std::cout << recv_str << std::endl;
     if (recv_str.find("% ") != std::string::npos){
-        // print prompt
+        output_shell(session_id, recv_str.substr(0, recv_str.find("%")));
+        // std::cout << recv_str.substr(0, recv_str.find("%")) << std::endl;
+        usleep(100000);
+        output_prompt(session_id);
+    }
+    else{
+        output_shell(session_id, recv_str);
     }
     
-    std::cout.write(bytes.data(), bytes_transferred);
-    std::cout.flush();
+    // std::cout.write(bytes.data(), bytes_transferred);
     
     bytes.fill(0);
     
@@ -42,11 +129,12 @@ void read_handler(const boost::system::error_code &ec,
         // std::cout.write(bytes.data(), bytes_transferred);
         // std::cout.flush();
         
-        usleep(10000);
+        usleep(100000);
         receive_prompt = true;
         
         std::string r = cmds[cmd_idx] + "\r\n";
-        std::cout << cmds[cmd_idx] << std::endl;
+        // std::cout << cmds[cmd_idx] << std::endl;
+        output_command(session_id, cmds[cmd_idx]);
         std::cout.flush();
 
         usleep(100000);
@@ -59,7 +147,7 @@ void read_handler(const boost::system::error_code &ec,
         // std::cout.flush();
         tcp_socket.async_read_some(buffer(bytes), read_handler);
     }
-
+    
     // clear received content
     // bytes.fill(0);
 
@@ -69,26 +157,13 @@ void read_handler(const boost::system::error_code &ec,
 
 void connect_handler(const boost::system::error_code &ec)
 {
-  if (!ec)
-  {
+  if (!ec){
+    // std::cout << "<script>document.getElementById('s1').innerHTML += '';</script>" << std::endl;
+    // std::cout.flush();
     tcp_socket.async_read_some(buffer(bytes), read_handler);
-    // if (receive_prompt != true){
-    //     sleep(0.01);
-    // }
-    // std::string r = "ls\r\n";
-    // std::cout << r << std::endl;
-    // receive_prompt = false;
-    // write(tcp_socket, buffer(r));
-    // tcp_socket.async_read_some(buffer(bytes), read_handler);
-    // std::string r = "ls\r\n";
-    // //   "GET / HTTP/1.1\r\nHost: theboostcpplibraries.com\r\n\r\n";
-    // std::cout << r << std::endl;
-    // write(tcp_socket, buffer(r));
-    // tcp_socket.async_read_some(buffer(bytes), read_handler);
-    // r = "who\r\n";
-    // std::cout << r << std::endl;
-    // write(tcp_socket, buffer(r));
-    // tcp_socket.async_read_some(buffer(bytes), read_handler);
+  }
+  else{
+        std::cerr << "can't connect to np_server" << std::endl;
   }
 }
 
@@ -106,6 +181,9 @@ int main(int argc, char* const argv[])
     return 1;
   }
 
+//   std::cout << "Content-type: text/html" << std::endl << std::endl;
+//   std::cout << webpage_template << std::endl;
+
   std::string cmd_line;
   std::ifstream input_file("./test_case/"+std::string(argv[3]));
   if(input_file.is_open()){
@@ -115,7 +193,8 @@ int main(int argc, char* const argv[])
     input_file.close();
   }
   else{
-      std::cout << "can't open " << argv[3] << std::endl;
+      std::cout << "<p> can't open " << argv[3] << "</p>" << std::endl;
+      return 1;
   }
   session_id = argv[4];
   tcp::resolver::query q{argv[1], argv[2]};
